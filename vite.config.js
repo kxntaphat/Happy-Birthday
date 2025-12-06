2363f118667c701f3d553526be73920e011b308b
kxntaphat <kantaphat2550@gmail.com>
2025-12-06 11:12:09 +0700

    build success

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: '/Happy-Birthday/', // 👈 เพิ่มบรรทัดนี้
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-  
->>>>>>> dfe7ca04f3c92f170ba9eb4a6105e213161b7637
+  base: '/Happy-Birthday/',
 })
